802ecb46f4a6dfb4d6c48d9bdce8be2405ee2ca9
檜原陽一郎 <hibarayoichiro@hibaruyouichirounoMacBook-puro.local>
2021-01-20 18:15:41 +0900

    production_v1

--- db/migrate/20201121140217_create_users.rb
+++ db/migrate/20201121140217_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
-      t.string :password_digest
+      t.string :name, default:'Jun'
+      t.string :email, default:'jun.pokepoke@docomo.ne.jp'
+      t.string :password_digest, default:'Yotto0416'
 
       t.timestamps
     end
